18a2446d28c8ef7f27307f706b43be3bbfbdfb4e
Bhuwan Thapa <thapabhuwan15@gmail.com>
2023-02-21 00:27:41 +0545

    Basic framework is created

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+require("./db/conn");
+
+const port = process.env.PORT || 3000;
+const staticPath = path.join(__dirname, "../public");
+const templatePath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.use(express.static(staticPath));
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+hbs.registerPartials(partialsPath);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port no. ${port}`);
+});

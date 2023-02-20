ea2f2dd13b9d4ecd1a04c02f50ed9896f5678fd9
Bhuwan Thapa <thapabhuwan15@gmail.com>
2023-02-21 00:46:32 +0545

    html and css added

--- src/app.js
+++ src/app.js
@@ -17,6 +17,9 @@ hbs.registerPartials(partialsPath);
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 app.get("/register", (req, res) => {
   res.render("register");
 });

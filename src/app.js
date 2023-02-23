8a698e074214be3283bdfb05fd8a2ff3838cdbbc
Bhuwan Thapa <thapabhuwan15@gmail.com>
2023-02-24 01:09:51 +0545

    bcrypt algorithm added

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const hbs = require("hbs");
+const bcrypt = require("bcryptjs");
 
 require("./db/conn");
 const Register = require("./models/registers");
@@ -11,6 +12,8 @@ const staticPath = path.join(__dirname, "../public");
 const templatePath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(staticPath));
 app.set("view engine", "hbs");
 app.set("views", templatePath);
@@ -22,8 +25,51 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login");
 });
+
 app.get("/register", (req, res) => {
   res.render("register");
+});
+app.post("/register", async (req, res) => {
+  try {
+    const password = req.body.password;
+    const cpassword = req.body.confirmPassword;
+
+    if (password === cpassword) {
+      const registerWebsite = new Register({
+        username: req.body.username,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        password: password,
+        confirmPassword: cpassword,
+      });
+
+      const registered = await registerWebsite.save();
+      res.status(201).render("index");
+    } else {
+      res.send("Passwords are not matching");
+    }
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userid = await Register.findOne({ username: username });
+
+    const isMatch = await bcrypt.compare(password, userid.password);
+
+    if (isMatch) {
+      res.status(201).render("index");
+    } else {
+      res.send("Invalid username or password");
+    }
+  } catch (err) {
+    res.status(400).send("Invalid username or password");
+  }
 });
 app.get("/booking", (req, res) => {
   res.render("booking");
